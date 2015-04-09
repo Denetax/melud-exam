d403f7d271c16660f8930809da6afe0442f4426c
Meilo <ludovic.elice@gmail.com>
2015-04-09 12:26:39 +0200

    Modifs css index

--- index.php
+++ index.php
@@ -20,7 +20,10 @@
 	<?php include 'web/header.php'; ?>
 	<!-- Fin template -->
 	<div class="container">
-		<h1>Jeux Concours - Faite ton affiche de cinéma d'horreur</h1>
+		<h2>Jeux Concours - Fait ton affiche de cinéma d'horreur</h2>
+		<p>
+			Minions ipsum jeje po kass butt tulaliloo. Poopayee tatata bala tu hahaha belloo! Gelatooo jiji po kass daa para tú la bodaaa. La bodaaa me want bananaaa! Po kass ti aamoo! Ti aamoo! tank yuuu! Underweaaar potatoooo bananaaaa gelatooo. Tatata bala tu me want bananaaa! Bananaaaa bananaaaa la bodaaa la bodaaa tulaliloo baboiii uuuhhh hana dul sae. Hana dul sae hahaha butt ti aamoo! Belloo! Bananaaaa po kass ti aamoo! Potatoooo pepete aaaaaah poopayee poopayee hana dul sae po kass. Para tú daa poulet tikka masala uuuhhh aaaaaah pepete hahaha poulet tikka masala poulet tikka masala. Bappleees me want bananaaa! Belloo! Baboiii jiji gelatooo. Wiiiii ti aamoo! Jeje aaaaaah belloo! Wiiiii po kass po kass para tú. Poulet tikka masala daa jeje po kass baboiii gelatooo. Gelatooo baboiii uuuhhh hahaha hana dul sae butt bee do bee do bee do bappleees tulaliloo poopayee. Hana dul sae jiji baboiii aaaaaah wiiiii belloo! Daa hahaha potatoooo aaaaaah poopayee. Bananaaaa gelatooo po kass butt po kass butt la bodaaa wiiiii po kass tulaliloo. Ti aamoo! gelatooo tatata bala tu jiji jiji. Pepete para tú daa hana dul sae tulaliloo poulet tikka masala belloo! Poopayee. Ti aamoo! belloo! Gelatooo gelatooo. Potatoooo me want bananaaa! Uuuhhh aaaaaah belloo! Tank yuuu! Chasy ti aamoo!
+		</p>
 		<?php
 			if($session)
 			{
@@ -32,10 +35,12 @@
 					$user = $request_user_executed->getGraphObject('Facebook\GraphUser');
 					?>
 					<a href="https://melud-exam.herokuapp.com/views/participe.php" class="btn btn-primary btn-lg">Je Participe</a>
-					<a href="https://melud-exam.herokuapp.com/views/vote.php" class="btn btn-info btn-lg">Je vote</a> 
+					<a href="https://melud-exam.herokuapp.com/views/vote.php" class="btn btn-info btn-lg">Je vote</a> <br><br>
 				<?php
 				} catch (Exception $e)
 				{
+					?>
+					<?php
 					$_SESSION = null;
 					session_destroy();
 					header('Location:index.php');
