68f81e3992f6f88be8ff936bc3b8906f569d23b8
Meilo <ludovic.elice@gmail.com>
2015-04-09 10:49:11 +0200

    test35

--- include/config.php
+++ include/config.php
@@ -2,10 +2,10 @@
 	error_reporting(E_ALL);
 	ini_set("display_error", 1);
 
-	use Facebook\FacebookSession;
+	/*use Facebook\FacebookSession;
 	use Facebook\FacebookRedirectLoginHelper;
 	use Facebook\FacebookRequest;
-	use Facebook\GraphObject;
+	use Facebook\GraphObject;*/
 
 	const APPID = "1574686139449224";
 	const APPSECRET = "6d8066b399193febf5f0a587443d4b48";
