99bbfe76201f5465dec80f6c79f6e896f786976e
Meilo <ludovic.elice@gmail.com>
2015-04-09 10:18:01 +0200

    test29

--- index.php
+++ index.php
@@ -1,5 +1,20 @@
 <?php
-	include 'include/include.php';
+	//include 'include/include.php';
+
+	error_reporting(E_ALL);
+	ini_set("display_error", 1);
+
+	require "SDKPHP/autoload.php";
+
+	use Facebook\FacebookSession;
+	use Facebook\FacebookRedirectLoginHelper;
+	use Facebook\FacebookRequest;
+	use Facebook\GraphObject;
+
+	session_start();
+
+	const APPID = "1574686139449224";
+	const APPSECRET = "6d8066b399193febf5f0a587443d4b48";
 
 	FacebookSession::setDefaultApplication(APPID, APPSECRET);
 
