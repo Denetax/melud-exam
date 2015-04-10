eadc9547dadce24ea256c66fda75b6c05a2ccca1
Meilo <ludovic.elice@gmail.com>
2015-04-10 08:46:11 +0200

    Modifs globale route css/js

--- include/include.php
+++ include/include.php
@@ -5,7 +5,7 @@
 	$bootstrapCSS = "https://melud-exam.herokuapp.com/web/css/bootstrap.min.css";
 	$bootstrapJS = "https://melud-exam.herokuapp.com/web/js/bootstrap.min.js";
 	$fbJS = "https://melud-exam.herokuapp.com/web/js/FBconfig.js";
-	$stylePerso = "https://melud-exam.herokuapp.com/web/css/styles.css"
+	$stylePerso = "https://melud-exam.herokuapp.com/web/css/styles.css";
 
 	/* Config Générale */
 
