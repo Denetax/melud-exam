e2cb1aa8d16115d2d8acded7aff288ac41a4d78a
Meilo <ludovic.elice@gmail.com>
2015-04-10 11:01:25 +0200

    modifs css

--- index.php
+++ index.php
@@ -22,8 +22,10 @@
 	<?php include 'web/header.php'; ?>
 	<!-- Fin template -->
 	<div class="container col-sm-10 col-sm-offset-1">
-		<div class="col-sm-8 col-sm-offset-2">
-			<img src="http://melud-exam.fr/views/web/images/title.png"/>
+		<div class="row">
+			<div class="col-sm-8 col-sm-offset-2">
+				<img src="http://melud-exam.fr/views/web/images/title.png"/>
+			</div>
 		</div>
 		<h2>Jeux Concours - Fait ton affiche de cinéma d'horreur</h2>
 		<p>
