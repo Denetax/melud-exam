0666439105248fed9ac9decfe0faeef2741e625e
Meilo <ludovic.elice@gmail.com>
2015-04-10 11:44:28 +0200

    modifs css

--- index.php
+++ index.php
@@ -38,10 +38,10 @@
 				<div class="row">
 					<div class="col-sm-4 col-sm-offset-4">
 						<div class="row">
-							<div class="col-sm-4 col-sm-offset-4">
+							<div class="col-sm-4">
 								<a href="https://melud-exam.herokuapp.com/views/participe.php" class="btn btn-primary btn-lg">Je Participe</a>
 							</div>
-							<div class="col-sm-4">
+							<div class="col-sm-4 col-sm-offset-4">
 								<a href="https://melud-exam.herokuapp.com/views/vote.php" class="btn btn-warning btn-lg">Je vote</a> <br><br>
 							</div>
 						</div>
