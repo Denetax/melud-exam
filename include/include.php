6717258d55f550b4fd462afb8a15e5c4e3579bbc
Meilo <ludovic.elice@gmail.com>
2015-04-10 08:39:18 +0200

    Modifs globale route css/js

--- include/include.php
+++ include/include.php
@@ -1,5 +1,12 @@
 <?php
 
+	/* Config css / js */
+
+	$bootstrapCSS = "melud-exam/web/css/bootstrap.min.css";
+	$bootstrapJS = "melud-exam/web/js/bootstrap.min.js";
+	$fbJS = "melud-exam/web/js/FBconfig.js";
+	$stylePerso = "melud-exam/web/css/styles.css"
+
 	/* Config Générale */
 
 	require 'config/config.php';
