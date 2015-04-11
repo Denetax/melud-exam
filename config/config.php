11471c3aa6e5e4457638ddca06fa9b59b92e9048
Denetax <mehdistocky@gmail.com>
2015-04-11 11:15:22 +0200

    Test sur config.php

--- config/config.php
+++ config/config.php
@@ -1,4 +1,5 @@
 <?php
+	echo "2 pas";
 	error_reporting(E_ALL);
 	ini_set("display_error", 1);
 
@@ -6,5 +7,5 @@
 
 	const APPID = "1574686139449224";
 	const APPSECRET = "6d8066b399193febf5f0a587443d4b48";
-	echo "2 pas";
+	//echo "2 pas";
 ?>
