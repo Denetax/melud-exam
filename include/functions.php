923ec7f287205222685357ba5f0055f03b984248
Denetax <mehdistocky@gmail.com>
2015-04-20 10:22:47 +0200

    var_dump($User);

--- include/functions.php
+++ include/functions.php
@@ -15,7 +15,7 @@
 				$user = $request_user_executed->getGraphObject('Facebook\GraphUser');
 				// $object = $response->getGraphObject();
 				// $user = $response->getGraphObject(GraphUser::className());
-				//var_dump($user);
+				var_dump($user);
 			}
 			catch (Exception $e)
 			{
