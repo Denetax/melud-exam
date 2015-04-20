ba21b02db904f4c8ce396701cbffcecb8e43e5b6
Denetax <mehdistocky@gmail.com>
2015-04-20 22:39:01 +0200

    uploadImage($session)

--- include/functions.php
+++ include/functions.php
@@ -51,18 +51,40 @@
 		}
 	}
 	
-	function uploadImage()
+	function uploadImage($session)
 	{
-		recup_user_picture_concours($session);
-		$link = "/".recup_user_id($session)."/photos";
-		$file = "https://melud-exam.herokuapp.com/web/img/example_image.png";  
+		// recup_user_picture_concours($session);
+		 $link = "/".recup_user_id($session)."/photos";
+		 $file = "https://melud-exam.herokuapp.com/web/img/example_image.png";  
 		
-		$response = (new FacebookRequest(
-			$session, 'POST', $link, array(
-				'source' => new CURLFile($file, 'image/png'),
-				'message' => 'User provided message'
-			)
-		))->execute()->getGraphObject();
-		echo("La fonction uploadImage OK");	
+		// $response = (new FacebookRequest(
+			// $session, 'POST', $link, array(
+				// 'source' => new CURLFile($file, 'image/png'),
+				// 'message' => 'User provided message'
+			// )
+		// ))->execute()->getGraphObject();
+		
+		try {
+
+				// Upload to a user's profile. The photo will be in the
+				// first album in the profile. You can also upload to
+				// a specific album by using /ALBUM_ID as the path     
+				$response = (new FacebookRequest(
+				  $session, 'POST', '/me/photos', array(
+					'source' => new CURLFile($file, 'image/png'),
+					'message' => 'User provided message'
+				  )
+				))->execute()->getGraphObject();
+
+				// If you're not using PHP 5.5 or later, change the file reference to:
+				// 'source' => '@/path/to/file.name'
+
+				echo "Posted with id: " . $response->getProperty('id');
+
+			} 
+			catch(FacebookRequestException $e) {
+				echo "Exception occured, code: " . $e->getCode();
+				echo " with message: " . $e->getMessage();
+			} 
 	}
 ?>
