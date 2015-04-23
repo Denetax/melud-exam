dc8d3af5e6c793af9c2ad2c875163e9e1c80b3a7
Meilo <ludovic.elice@gmail.com>
2015-04-23 10:29:26 +0200

    upload albums

--- include/functions.php
+++ include/functions.php
@@ -76,7 +76,7 @@
 	
 	function uploadImage($session, $file)
 	{
-		$link = "/".recup_user_id($session)."/photos";
+		$link = "/".recup_user_id($session)."/albums";
 		// $file = $_FILES['fichier']['name'];
 		// $file_tmp = $_FILES['fichier']['tmp_name'];
 
