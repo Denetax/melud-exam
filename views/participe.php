82835f9f9175a83e0f63ed711b1e00461c97088e
Meilo <ludovic.elice@gmail.com>
2015-05-12 17:58:41 +0200

    test

--- views/participe.php
+++ views/participe.php
@@ -31,6 +31,7 @@
 				if($session)
 				{ ?>
 					<?php 
+					echo $session;
 					recup_user_picture_concours($session);
 					session_auto($session);
 					if (isset($_POST['participer']) && $_FILES['fichier']['name'] != "" && $_POST['nameAlbum'] != "" && $_POST['descAlbum'] != "")
