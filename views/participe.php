3ce56502e5da677754b8e1a14648b2787baa831f
Meilo <ludovic.elice@gmail.com>
2015-05-13 15:44:17 +0200

    test

--- views/participe.php
+++ views/participe.php
@@ -32,7 +32,10 @@
 				{ ?>
 					<?php
 					$test = recup_user_picture_concours($session);
-					var_dump($test); 
+					var_dump($test);
+					for ($i=0; $i < count($test) ; $i++) { 
+						var_dump($i);
+					}
 					// foreach ($variable as $test) 
 					// {
 					// 	var_dump($variable);
