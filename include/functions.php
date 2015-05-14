da6239c9f862b1ca414de6935c9467585d5d2cb0
Denetax <mehdistocky@gmail.com>
2015-05-14 23:49:18 +0200

    Last Push avant demain
    Test de faire un select si il me retourne une erreur je comprend plus
    rien

--- include/functions.php
+++ include/functions.php
@@ -8,13 +8,14 @@
 	function connexionBdd()
 	{		
 		$dbconn1 = pg_connect("host=ec2-54-217-202-108.eu-west-1.compute.amazonaws.com port=5432 dbname=d5iqngvvkvdj0o user=vcgyjwcpqrizgf password= DlgzzsaQvO0PamJBLqxj5fxlKK") or die('connection failed');
-		$result = pg_query($dbconn1,"CREATE TABLE utilisateur2(id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,tokenUser VARCHAR(100),href VARCHAR(100))");
+		//$result = pg_query($dbconn1,"CREATE TABLE utilisateur2(id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,tokenUser VARCHAR(100),href VARCHAR(100))");
+		$result = pg_query($dbconn1, "SELECT tokenUser, href FROM utilisateur");
 		if (!$result) {
 		  echo "Une erreur s'est produite.\n";
 		}
 		//$result = pg_query($dbconn1, "DROP TABLE utilisateur");
 		//pg_query($dbconn1, "INSERT INTO utilisateur (tokenUser, href VALUES ('oghruoufeu651781', 'http://blablabla.fr'))");
-		//$result = pg_query($dbconn1, "SELECT tokenUser, href FROM utilisateur");
+		
 		//while ($row = pg_fetch_row($result)) {
 		//echo "TokenUser: $row[0]  href: $row[1]";
 		//echo "<br />\n";	
