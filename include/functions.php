44a9292fda356b6d3575631de89874f4c1b8ec92
Denetax <mehdistocky@gmail.com>
2015-05-14 22:26:04 +0200

    Test d'affichage d'une erreur de base

--- include/functions.php
+++ include/functions.php
@@ -7,7 +7,7 @@
 	
 	function connexionBdd()
 	{		
-		$dbconn1 = pg_connect("host=ec2-54-217-202-108.eu-west-1.compute.amazonaws.com port=5432 dbname=d5iqngvvkvdj0o user=vcgyjwcpqrizgf password= DlgzzsaQvO0PamJBLqxj5fxlKK") or die('connection failed');
+		$dbconn1 = pg_connect("host=testdefail/ec2-54-217-202-108.eu-west-1.compute.amazonaws.com port=5432 dbname=d5iqngvvkvdj0o user=vcgyjwcpqrizgf password= DlgzzsaQvO0PamJBLqxj5fxlKK") or die('connection failed');
 		return $dbconn1;
 		//$result = pg_query_params($dbconn1, 'CREATE TABLE utilisateur(id INT PRIMARY KEY NOT NULL,tokenUser VARCHAR(100),href VARCHAR(100))');
 	}
