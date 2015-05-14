383a9dacd0cf873dc77b74b9e5a09e3b99aed816
Denetax <mehdistocky@gmail.com>
2015-05-14 23:37:16 +0200

    Erreur High level O_o
    je crois qu'il faut que j'aille me coucher xD

--- include/functions.php
+++ include/functions.php
@@ -7,19 +7,15 @@
 	
 	function connexionBdd()
 	{		
-		echo "1";
 		$dbconn1 = pg_connect("host=ec2-54-217-202-108.eu-west-1.compute.amazonaws.com port=5432 dbname=d5iqngvvkvdj0o user=vcgyjwcpqrizgf password= DlgzzsaQvO0PamJBLqxj5fxlKK") or die('connection failed');
-		echo "2";
 		$result = pg_query($dbconn1,"CREATE TABLE utilisateur2(id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,tokenUser VARCHAR(100),href VARCHAR(100))");
-		echo "3";
+
 		//$result = pg_query($dbconn1, "DROP TABLE utilisateur");
 		//pg_query($dbconn1, "INSERT INTO utilisateur (tokenUser, href VALUES ('oghruoufeu651781', 'http://blablabla.fr'))");
 		//$result = pg_query($dbconn1, "SELECT tokenUser, href FROM utilisateur");
 		//while ($row = pg_fetch_row($result)) {
 		//echo "TokenUser: $row[0]  href: $row[1]";
-		//echo "<br />\n";
-}
-		
+		//echo "<br />\n";	
 	}	
 	function session_auto($session)
 	{
