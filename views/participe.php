7eda4a19b975dc901b2776080d36e004cf2e23e2
Meilo <ludovic.elice@gmail.com>
2015-05-15 15:19:22 +0200

    design css mod

--- views/participe.php
+++ views/participe.php
@@ -20,13 +20,13 @@
 		<!-- template header -->
 		<?php include 'web/header.php'; ?>
 		<!-- Fin template -->
-		<div class="container col-sm-10 col-sm-offset-1">
+		<div class="container col-sm-12">
 			<div class="row">
 				<div class="col-sm-8 col-sm-offset-2">
 					<img src="http://melud-exam.fr/views/web/images/title.png"/>
 				</div>
 			</div>
-			<h2>Jeux Concours - Fais ton affiche de cinéma d'horreur</h2>
+			<h2>Jeux Concours</h2>
 			<?php
 				if($session)
 				{ ?>
--- views/vote.php
+++ views/vote.php
@@ -21,7 +21,7 @@
 		<!-- template header -->
 		<?php include 'web/header.php'; ?>
 		<!-- Fin template -->
-		<div class="container col-sm-10 col-sm-offset-1">
+		<div class="container col-sm-12">
 			<div class="row">
 				<div class="col-sm-8 col-sm-offset-2">
 					<img src="http://melud-exam.fr/views/web/images/title.png"/>
