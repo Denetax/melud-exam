6b51473d0b7394da5cfe19aa20684f1cfc60a0cb
Denetax <mehdistocky@gmail.com>
2015-05-15 14:37:45 +0200

    Rajout du PORT

--- include/functions.php
+++ include/functions.php
@@ -7,9 +7,9 @@
 	
 	function connexionBdd()
 	{		
-		//pgsql:host=ec2-54-217-202-108.eu-west-1.compute.amazonaws.com;port=5432;dbname=testdb;user=bruce;password=mypass
+		//pgsql:host=ec2-54-217-202-108.eu-west-1.compute.amazonaws.com;port=5432;dbname=d5iqngvvkvdj0o;user=vcgyjwcpqrizgf;password=DlgzzsaQvO0PamJBLqxj5fxlKK
 		try {
-			  $db = new PDO("pgsql:host=ec2-54-217-202-108.eu-west-1.compute.amazonaws.com;dbname=d5iqngvvkvdj0o", "vcgyjwcpqrizgf", "DlgzzsaQvO0PamJBLqxj5fxlKK");
+			  $db = new PDO("pgsql:host=ec2-54-217-202-108.eu-west-1.compute.amazonaws.com;port=5432;dbname=d5iqngvvkvdj0o;user=vcgyjwcpqrizgf;password=DlgzzsaQvO0PamJBLqxj5fxlKK");
 			  echo 'Connexion OK';
 			}
 			catch(PDOException $e) {
