f3e1deb93ff1854ffaf4cf256412321d455c9430
Denetax <mehdistocky@gmail.com>
2015-05-31 14:16:24 +0200

    Maj felicitation

--- views/felicitation.php
+++ views/felicitation.php
@@ -1,1 +1,44 @@
-Bonjour, <?php echo htmlspecialchars($_POST['inputSrc']); ?>.
+<?php
+	include '../include/functions.php';
+	require '../config/config.php';
+	require '../SDKPHP/autoload.php';
+	require '../controllers/SessionController.php';
+?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<link rel="stylesheet" href="https://melud-exam.herokuapp.com/web/css/bootstrap.min.css">
+		<link rel="stylesheet" href="https://melud-exam.herokuapp.com/web/css/styles.css">
+		<script src="https://melud-exam.herokuapp.com/web/js/jquery-2.1.4.min.js"></script>
+		<script src="https://melud-exam.herokuapp.com/web/js/bootstrap.min.js"></script>
+		<script src="https://melud-exam.herokuapp.com/web/js/FBconfig.js"></script>
+		<script src="https://melud-exam.herokuapp.com/web/js/app.js"></script>
+		<title>Jeux Coucours</title>
+	</head>
+	<body>
+		<!-- template header -->
+		<?php //include 'web/header.php'; ?>
+		<!-- Fin template -->
+		
+		<div class="container col-sm-12">
+			<?php
+				if($session)
+				{ ?>
+				<ul class="nav nav-pills">
+					<li role="presentation"><a href="https://melud-exam.herokuapp.com">Retour</a></li>
+					<li> Bonjour, <?php echo htmlspecialchars($_POST['inputSrc']); ?></li>
+				</ul>
+				<?php 
+				}
+				else
+				{
+					$loginUrl = $helper->getLoginUrl(['email','user_photos','publish_actions']);
+					echo "<a href=".$loginUrl." class='btn btn-primary btn-lg'>Se Connecter</a><br><br>";
+				}
+			?><br>
+			<div class="fb-like" data-share="true" data-width="450" data-show-faces="true"></div>
+		</div>
+		<!-- template footer -->
+		<?php //include 'web/footer.php'; ?>
+		<!-- Fin template -->
