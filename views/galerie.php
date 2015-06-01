151c973fe9a9acf2b4df0a35635ee97f003443b5
Meilo <ludovic.elice@gmail.com>
2015-06-01 15:45:53 +0200

    test

--- views/galerie.php
+++ views/galerie.php
@@ -34,6 +34,7 @@
 			foreach ($req as $value) { ?>
 			<div class="col-sm-4">
 				<img src="<?php echo $value['href'] ?>" alt="">
+				<div class="fb-like" data-href="<?php echo $value['href'] ?>" data-layout="button_count" data-action="like" data-show-faces="true" data-share="true"></div>
 			</div>
 			<?php } ?><br><br>
 			<div class="fb-like" data-share="true" data-width="450" data-show-faces="true"></div>
