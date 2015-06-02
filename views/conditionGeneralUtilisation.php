5c2c09dff13506a48a5de0290485dbed3746c619
Denetax <mehdistocky@gmail.com>
2015-06-02 15:26:52 +0200

    Mise a jour css

--- views/conditionGeneralUtilisation.php
+++ views/conditionGeneralUtilisation.php
@@ -21,31 +21,31 @@
 		<!-- Fin template -->
 		<div class="container col-sm-12">
 						<h1 style="text-align:center;">Condition d'utilisation Melud-exam</h1>
-						<p>
+						<p style="font-size:12px;">
 						Le présent document a pour objet de d'finir les modalités et conditions dans lesquelles d'une part,  Melud-exam , ci-aprés dénommé l'EDITEUR, 
 						met à la disposition de ses utilisateurs le site,
 						et les services disponibles sur le site et d’autre part, la manière par laquelle l’utilisateur accède au site et utilise ses services.
-						Toute connexion au site est subordonnée au respect des présentes conditions.<br/>
+						Toute connexion au site est subordonnée au respect des présentes conditions.<br/><br/>
 						Pour l’utilisateur, le simple accès au site de l’EDITEUR à l’adresse URL suivante Melud-exam.fr
-						implique l’acceptation de l’ensemble des conditions décrites ci-après.<br/>
+						implique l’acceptation de l’ensemble des conditions décrites ci-après.<br/><br/>
 						
-						Le site  Melud-exam.fr  peut contenir des liens hypertextes vers d’autres sites présents sur le réseau Internet.<br/>
+						Le site  Melud-exam.fr  peut contenir des liens hypertextes vers d’autres sites présents sur le réseau Internet.<br/><br/>
 						Les liens vers ces autres ressources vous font quitter le site Melud-exam
-						Il est possible de créer un lien vers la page de présentation de ce site sans autorisation expresse de l’EDITEUR.<br/>
+						Il est possible de créer un lien vers la page de présentation de ce site sans autorisation expresse de l’EDITEUR.<br/><br/>
 						Aucune autorisation ou demande d’information préalable ne peut être exigée par l’éditeur à l’égard d’un site qui souhaite établir un lien vers le site de l’éditeur.
 						Il convient toutefois d’afficher ce site dans une nouvelle fenêtre du navigateur.<br/>
 						Cependant,l’EDITEUR se réserve le droit de demander la suppression d’un lien qu’il estime non conforme à l’objet du site Melud-exam.fr
 						
-						Les informations et/ou documents figurant sur ce site et/ou accessibles par ce site proviennent de sources considérées comme étant fiables.<br/>
+						Les informations et/ou documents figurant sur ce site et/ou accessibles par ce site proviennent de sources considérées comme étant fiables.<br/><br/>
 
-						Toutefois, ces informations et/ou documents sont susceptibles de contenir des inexactitudes techniques et des erreurs typographiques.<br/>
+						Toutefois, ces informations et/ou documents sont susceptibles de contenir des inexactitudes techniques et des erreurs typographiques.<br/><br/>
 						L’EDITEUR se réserve le droit de les corriger, dès que ces erreurs sont portées à sa connaissance.
 
-						Il est fortement recommandé de vérifier l’exactitude et la pertinence des informations et/ou documents mis à disposition sur ce site.<br/>
+						Il est fortement recommandé de vérifier l’exactitude et la pertinence des informations et/ou documents mis à disposition sur ce site.<br/><br/>
 						Les informations et/ou documents disponibles sur ce site sont susceptibles d’être modifiés à tout moment, et peuvent avoir fait l’objet de mises à jour.
-						En particulier, ils peuvent avoir fait l’objet d’une mise à jour entre le moment de leur téléchargement et celui où l’utilisateur en prend connaissance.<br/>
+						En particulier, ils peuvent avoir fait l’objet d’une mise à jour entre le moment de leur téléchargement et celui où l’utilisateur en prend connaissance.<br/><br/>
 						L’utilisation des informations et/ou documents disponibles sur ce site se fait sous l’entière et seule responsabilité de l’utilisateur, qui assume la totalité des conséquences pouvant en découler,
-						sans que l’EDITEUR puisse être recherché à ce titre, et sans recours contre ce dernier.<br/>
+						sans que l’EDITEUR puisse être recherché à ce titre, et sans recours contre ce dernier.<br/><br/>
 
 						L’EDITEUR ne pourra en aucun cas être tenu responsable de tout dommage de quelque nature 
 						qu’il soit résultant de l’interprétation ou de l’utilisation des informations et/ou documents disponibles sur ce site.
