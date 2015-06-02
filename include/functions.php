ff89f12fed15dc8966a073db8cbc05f7f676ede9
Meilo <ludovic.elice@gmail.com>
2015-06-02 11:31:31 +0200

    test

--- include/functions.php
+++ include/functions.php
@@ -8,7 +8,7 @@
 	function connexionBdd()
 	{		
 		$dbconn2 = pg_connect("host=ec2-54-217-202-108.eu-west-1.compute.amazonaws.com port=5432 dbname=d83d3aeifsc9ir user=qvgrnmrngeochj password= ByPWUf6LDRo4Cflah_kraHAExL") or die('connection failed');
-		$result = pg_query($dbconn2,"CREATE TABLE db_concours(id SERIAL PRIMARY KEY NOT NULL,tokenUser VARCHAR(255),href VARCHAR(255),nomAlbum VARCHAR(100))");
+		// $result = pg_query($dbconn2,"CREATE TABLE db_concours(id SERIAL PRIMARY KEY NOT NULL,tokenUser VARCHAR(255),href VARCHAR(255),nomAlbum VARCHAR(100))");
 		return $dbconn2;
 	}
 
@@ -66,7 +66,7 @@
 	function verif_user_id($id)
 	{
 		$bdd = connexionBdd();
-		$result = Query($bdd,"SELECT * FROM concours WHERE tokenUser = '$id'" );
+		$result = Query($bdd,"SELECT * FROM db_concours WHERE tokenUser = '$id'" );
 		
 		$req = pg_fetch_all($result);
 		
