50bcb79fa9958197cdffc143a6ad67322e2976ad
Denetax <mehdistocky@gmail.com>
2015-06-02 15:32:49 +0200

    Mise a jour condition

--- views/conditionGeneralUtilisation.php
+++ views/conditionGeneralUtilisation.php
@@ -21,7 +21,7 @@
 		<!-- Fin template -->
 		<div class="container col-sm-12">
 						<h1 style="text-align:center;">Condition d'utilisation Melud-exam</h1>
-						<p style="font-size:12px;">
+						<p style="font-size:12px;text-align:left;">
 						Le présent document a pour objet de d'finir les modalités et conditions dans lesquelles d'une part,  Melud-exam , ci-aprés dénommé l'EDITEUR, 
 						met à la disposition de ses utilisateurs le site,
 						et les services disponibles sur le site et d’autre part, la manière par laquelle l’utilisateur accède au site et utilise ses services.
