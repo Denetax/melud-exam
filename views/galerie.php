dc837ee6a74e8795e9d0b16a61718f56fd472df1
Denetax <mehdistocky@gmail.com>
2015-06-02 18:29:18 +0200

    Mise en forme du code Galerie

--- views/galerie.php
+++ views/galerie.php
@@ -7,9 +7,9 @@
 <!DOCTYPE html>
 <html>
 	<head>
-		<meta charset="UTF-8">
-		<link rel="stylesheet" href="https://melud-exam.herokuapp.com/web/css/bootstrap.min.css">
-		<link rel="stylesheet" href="https://melud-exam.herokuapp.com/web/css/styles.css">
+		<meta charset="UTF-8"/>
+		<link rel="stylesheet" href="https://melud-exam.herokuapp.com/web/css/bootstrap.min.css"/>
+		<link rel="stylesheet" href="https://melud-exam.herokuapp.com/web/css/styles.css"/>
 		<script src="https://melud-exam.herokuapp.com/web/js/jquery-2.1.4.min.js"></script>
 		<script src="https://melud-exam.herokuapp.com/web/js/bootstrap.min.js"></script>
 		<script src="https://melud-exam.herokuapp.com/web/js/FBconfig.js"></script>
@@ -17,31 +17,28 @@
 		<title>Jeux Coucours</title>
 	</head>
 	<body>
-		<!-- template header -->
-		<?php //include 'web/header.php'; ?>
-		<!-- Fin template -->
-		
 		<div class="container col-sm-12">
 			<ul class="nav nav-pills">
 				<li><a href="https://melud-exam.herokuapp.com">Retour</a></li>
 				<li><a href="https://melud-exam.herokuapp.com/views/conditionGeneralUtilisation.php">Condition Géneral D'utilisation</a></li>
 			</ul>
+			
 			<h1 style="color:#000">Galerie d'Images.</h1>
 			<?php 
-
 			$bdd = connexionBdd();
 			$result = Query($bdd,"SELECT * FROM db_concours" );
 			$req = pg_fetch_all($result);
 
-			foreach ($req as $value) { ?>
-			<div id="photoGalerie"class="col-sm-4">
-				<img src="<?php echo $value['href'] ?>" alt="">
-			</div>
-			<?php } ?>
+			foreach ($req as $value) 
+			{ 
+			?>
+				<div id="photoGalerie"class="col-sm-4">
+					<img src="<?php echo $value['href'] ?>"/>
+				</div>
+			<?php 
+			}
+			?>
 		</div>
-		<!-- template footer -->
-		<?php //include 'web/footer.php'; ?>
-		<!-- Fin template -->
 		<div id="myModalGalerie" class="modal fade">
 			<div class="modal-dialog">
 				<div class="modal-content">
@@ -50,7 +47,7 @@
 						<h4 class="modal-title"></h4>
 					</div>
 					<div class="modal-body">
-						<img id="imgSelected" src="" width="100%" />
+						<img id="imgSelected" width="100%" />
 					</div>
 					<div class="modal-footer">
 						
