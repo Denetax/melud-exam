231898706e9e98197a5c0f1d2f431b5635f588a6
Denetax <mehdistocky@gmail.com>
2015-06-03 09:29:28 +0200

    felicitation mise en forme

--- views/felicitation.php
+++ views/felicitation.php
@@ -7,9 +7,9 @@
 <!DOCTYPE html>
 <html>
 	<head>
-		<meta charset="UTF-8">
-		<link rel="stylesheet" href="https://melud-exam.herokuapp.com/web/css/bootstrap.min.css">
-		<link rel="stylesheet" href="https://melud-exam.herokuapp.com/web/css/styles.css">
+		<meta charset="UTF-8"/>
+		<link rel="stylesheet" href="https://melud-exam.herokuapp.com/web/css/bootstrap.min.css"/>
+		<link rel="stylesheet" href="https://melud-exam.herokuapp.com/web/css/styles.css"/>
 		<script src="https://melud-exam.herokuapp.com/web/js/jquery-2.1.4.min.js"></script>
 		<script src="https://melud-exam.herokuapp.com/web/js/bootstrap.min.js"></script>
 		<script src="https://melud-exam.herokuapp.com/web/js/FBconfig.js"></script>
@@ -17,10 +17,6 @@
 		<title>Jeux Coucours</title>
 	</head>
 	<body>
-		<!-- template header -->
-		<?php //include 'web/header.php'; ?>
-		<!-- Fin template -->
-		
 		<div class="container col-sm-12">
 			<?php
 				if($session)
@@ -52,6 +48,5 @@
 				
 			?><br>
 		</div>
-		<!-- template footer -->
-		<?php //include 'web/footer.php'; ?>
-		<!-- Fin template -->
+	</body>
+</html>
