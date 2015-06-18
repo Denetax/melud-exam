932d6b0ff059578052f25d39bfca80c6ec687cf4
Denetax <mehdistocky@gmail.com>
2015-06-18 18:39:16 +0200

    mise en forme album

--- views/participe.php
+++ views/participe.php
@@ -32,7 +32,7 @@
 						foreach ($lesAlbums as $tof) 
 							{
 								?>
-									<div class="col-sm-3">
+									<div class="col-sm-3 MiseEnFormeAlbum">
 										<div></div>
 										<div id="<?php echo $tof->id ?>"><?php echo $tof->name ?></div> 
 									</div>
