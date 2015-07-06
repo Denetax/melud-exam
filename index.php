532e2fb255ad60e299a642d8e595da2d3f34f246
Meilo <ludovic.elice@gmail.com>
2015-07-06 09:27:18 +0200

    nouveau index

--- index.php
+++ index.php
@@ -7,10 +7,7 @@
 	use Facebook\FacebookRequest;
 	use Facebook\GraphObject;
 
-	$conn=connexionBdd();
-	$result = Query($conn,"SELECT * FROM db_concours ORDER BY RANDOM() LIMIT 3");
-
-
+	// connexionBdd();
 ?>
 <!DOCTYPE html>
 <html>
@@ -28,56 +25,53 @@
 				<div class="col-sm-8">
 					<?php if($session == "")
 					{
-						$loginUrl = $helper->getLoginUrl(['user_photos','publish_actions','email']);
-						$loginUrl2 = $helper->getLoginUrl(['email']);
-                    ?>
+						$loginUrl = $helper->getLoginUrl(['user_photos','publish_actions']); ?>
 						<ul class="nav nav-pills">
-                            <li><a href="<?php echo $loginUrl ?>">Je Participe</a></li>
+							<li><a href="<?php echo $loginUrl ?>">Je Participe</a></li>
 							<li><a href="https://melud-exam.herokuapp.com/views/galerie.php">Photos participante</a></li>
-							<li><a href="<?php echo $loginUrl2 ?>">Je vote</a></li>
 							<li><a href="https://melud-exam.herokuapp.com/views/conditionGeneralUtilisation.php">Mentions Légales</a></li>
 						</ul>
-					<?php
+					<?php 
 					}
 					else
-					{
+					{ 
 						session_auto($session); ?>
 						<ul class="nav nav-pills">
 							<li role="presentation"><a href="https://melud-exam.herokuapp.com/views/participe.php">Je Participe</a></li>
 							<li role="presentation"><a href="https://melud-exam.herokuapp.com/views/vote.php">Je vote</a></li>
 							<li><a href="https://melud-exam.herokuapp.com/views/conditionGeneralUtilisation.php">Mentions Légales</a></li>
 						</ul>
-					<?php
-					}
-					?>
+					<?php 
+					} 
+					?>	
 				</div>
 			</div>
-			<div class="row" style="height:300px; margin-top:20px;">
-				<div class="col-sm-12">
+			<div class="row bandeImage" style="height:300px; margin-top:20px;">
+				<div class="col-sm-8 col-sm-offset-2">
 					<h1 style="font-size:80px">Melud</h1>
 					<p>
-						Bienvenue sur le jeux concours Melud dédié à la photographie.
-						Le principe est simple,
+						Bienvenue sur le jeux concours Melud dédié à la photographie. <br>
+						Le principe est simple, 
 						prend toi en photo dans un magnifique paysage
-						pour participer au coucours.
+						pour participe au coucours.
 					</p>
-					<div class="row ">
-					<?php
-						while ($row = pg_fetch_row($result)) {
-						?>
-						<div class="col-md-offset-1 col-sm-3 miseEnFormeAccueilPhoto">
-							<img style="width:100%;" src=" <?php echo $row[2] ?> "/>
-						</div>
-
-					<?php }
-					?>
-					</div>
-
 				</div>
 			</div>
-			<div class="row" id="miseEnFormeBoxFacebookAcueil">
-				<div class="fb-like col-md-offset-3 col-sm-6" data-share="true" data-width="450" data-show-faces="true"></div>
-			</div>
+			<!-- <div class="row">
+				<div class="col-sm-3">
+					<img src="https://melud-exam.herokuapp.com/web/img/80652197.jpg"/>
+				</div>
+				<div class="col-sm-3">
+					<img src="https://melud-exam.herokuapp.com/web/img/82e2ae94.jpg"/>
+				</div>
+				<div class="col-sm-3">
+					<img src="https://melud-exam.herokuapp.com/web/img/photo-1415298910336-daa47babb3cc.jpg"/>
+				</div>
+				<div class="col-sm-3">
+					<img src="https://melud-exam.herokuapp.com/web/img/photo-1428954376791-d9ae785dfb2d.jpg"/>
+				</div>
+			</div> --><br/>
+			<div class="fb-like" data-share="true" data-width="450" data-show-faces="true"></div>
 		</div>
 	</body>
 </html>
