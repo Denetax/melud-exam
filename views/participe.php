d70fbae874ee40cbe28a874912b69819bcc89328
Meilo <ludovic.elice@gmail.com>
2015-07-23 16:07:01 +0200

    modification participe.php

--- views/participe.php
+++ views/participe.php
@@ -110,7 +110,7 @@
 							}
 						}
 					}else{ ?>
-					<form enctype="multipart/form-data" method="POST" action="https://melud-exam.herokuapp.com/views/participe.php">
+					<form enctype="multipart/form-data" method="POST" action="https://melud-exam.herokuapp.com">
 						<div class="row">
 							<div class="col-sm-6">
 								<div class="input-group input-group-lg">
