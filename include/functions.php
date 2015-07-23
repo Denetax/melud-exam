d67bf489652198238eb2644a64f51047e052d5fe
Meilo <ludovic.elice@gmail.com>
2015-07-23 16:39:48 +0200

    modif page vote

--- include/functions.php
+++ include/functions.php
@@ -253,4 +253,14 @@
 		$request2 = $response->execute();
  		$graphObject2 = $request2->getGraphObject();
 	}
+
+	function get_facebook_activite($url)
+	{
+		$fql = "SELECT%20url,normalized_url,share_count,like_count,
+		comment_count,total_count,commentsbox_count,comments_fbid,click_count
+		%20FROM%20link_stat%20WHERE%20url='$url'";
+		$apifql="http://graph.facebook.com/fql?q=$fql";
+		$graph_result_json = file_get_contents($apifql);
+		return json_decode($graph_result_json);
+	}
 ?>
